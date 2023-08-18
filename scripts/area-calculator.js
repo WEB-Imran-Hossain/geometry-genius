ec4329c18ab8086c3e2bd8a228c1f4867ad74eef
Imran <webimran2021@gmail.com>
2023-08-18 18:02:26 +0600

    added ractangele calculation

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -13,12 +13,34 @@ function calculateTriangleArea() {
   //   clear the height value text
   document.getElementById("triangle-height").value = "";
 
-//   get data from calculate
+  //   get data from calculate
   const area = 0.5 * base * height;
 
-//   show triangle area
-const areaSpan = document.getElementById("triangle-area")
-areaSpan.innerText = area
-console.log(area);
+  //   show triangle area
+  const areaSpan = document.getElementById("triangle-area");
+  areaSpan.innerText = area;
+}
 
+// ----------------------------------------------------------
+function calculateRectangleArea() {
+  //    get Rectangle width value
+  const widthField = document.getElementById("rectangle-width");
+  const widthValueText = widthField.value;
+  const width = parseFloat(widthValueText);
+  //   clear the width value text
+  document.getElementById("rectangle-width").value = "";
+
+  //   get rectangle length value
+  const lengthField = document.getElementById("rectangle-length");
+  const lengthValueText = lengthField.value;
+  const length = parseFloat(lengthValueText);
+  //   clear the length value text
+  document.getElementById("rectangle-length").value = "";
+
+  //   get data from calculate
+  const area = width * length;
+
+  //   show triangle area
+  const areaSpan = document.getElementById("rectangle-area");
+  areaSpan.innerText = area;
 }
